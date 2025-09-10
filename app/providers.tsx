6ff5154b1239aa5cb8fd7e3b5eb402500a91b182
Teem001 <sulaimanolayinkahuzain@gmail.com>
2025-09-10 20:10:56 +0100

    fix layout error

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,10 @@
 "use client"
-
 import { AuthProvider } from "@/components/auth/auth-provider"
 import { Header } from "@/components/layout/header"
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <AuthProvider children={children}>
+        <AuthProvider>
             <Header />
             {children}
         </AuthProvider>
